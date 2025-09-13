91458c39e87cea9e7b50e8e6704cd4b7b008979a
Kennedy Michelle Kirk <kirkkm22@wfu.edu>
2025-09-12 21:16:04 -0400

    Refactor lens aperture parameters and plotting code fixed bug

--- image_formation/lens_aperture_params.py
+++ image_formation/lens_aperture_params.py
@@ -9,19 +9,19 @@ def thin_lens(z0, f):
 
 focal_length1= 3 # in mm
 focal_length2= 9 # in mm
-focal_length3=50
+focal_length3= 50
 focal_length4 =200
 
 z0_min =1.1
-z0_max =104 # in mm
+z0_max = 500 # in mm
 z0_point = 4
 
 ## Generate object distance values
 z0_values = np.linspace(z0_min, z0_max, int((z0_max-z0_min)*z0_point))
 
 ## Plotting
-plt.figure(figsize=(12, 8))
-colors = ['r', 'g', 'b', 'm']
+plt.figure(figsize=(10, 6))
+colors = ['r', 'g', 'b', 'k']
 ## Plot for each focal length
 for f, color in zip([focal_length1, focal_length2, focal_length3, focal_length4], colors):
     z0 = z0_values[z0_values > f]
@@ -42,18 +42,18 @@ plt.show()
 # fnumbers 
 f_numbers = [1.4,1.8,2.8,4.0]
 #focal lengths 
-f_values = np.linespace(20,650,500)
+f_values = np.linspace(20, 650, 500)
 
 
 lens_examples = [(24,1.4, "24mm f/1.4"),(50,1.8,"50mm f/1.8"),(70,2.8,"85mm f/2.8"),(200,4.0,"200mm f/4.0") ,(400,2.8,"400mm f/2.8"),(600,4.0,"600mm f/4.0")]
-for f_numbers,color in zip(f_numbers,colors):
-    D = f_values/f_numbers
-    plt.plot(f_values,D,label=f'f/{f_numbers}',color=color)
+for f_number,color in zip(f_numbers,colors):
+    D = f_values/f_number
+    plt.plot(f_values,D,label=f'f/{f_number}',color=color)
 
 
 # Plot lens examples
-for f,f_numbers,label in lens_examples:
-     D = f/f_numbers
+for f,fnum,label in lens_examples:
+     D = f/fnum
      plt.scatter(f,D,label=label,s=80,edgecolors='k')
 
 
